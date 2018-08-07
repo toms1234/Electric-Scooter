517f4c25a05f4ab8fe6c4ad55c50a833c1e10fb9
Ruben <ruben.van.opstal@gmail.com>
2018-08-07 10:51:04 +0200

    Add files via upload

---
+++ 2 Mathematics/E-Scooter/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define M_PI 3.14159265358979323846264338327950288
+
+const double batteryVolt = (6 * ((4.2 + 3.7) / 2));
+const int motorRPM = 4000;		// at half voltage = 6S
+const int motorWatt = 1125;		// watts
+
+const double wheelDia = 0.2;	// meters
+
+const double kmh_mph_ratio = 0.6213712;
+
+const double gravity = 9.81;				// m/s^2
+const double scooterWeight = 10;	// kg
